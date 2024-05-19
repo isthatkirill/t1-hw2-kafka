368edfdaafe7702a61caec98bbeef80caf59b6f2
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2024-05-19 14:57:15 +0300

    refactor: module metrics producer added

--- metrics-producer/src/main/java/isthatkirill/hwtwokafka/producer/ProducerServiceApp.java
+++ metrics-producer/src/main/java/isthatkirill/hwtwokafka/producer/ProducerServiceApp.java
@@ -1,13 +1,13 @@
-package isthatkirill.hwtwokafka;
+package isthatkirill.hwtwokafka.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HwTwoKafkaApplication {
+public class ProducerServiceApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HwTwoKafkaApplication.class, args);
+		SpringApplication.run(ProducerServiceApp.class, args);
 	}
 
 }
