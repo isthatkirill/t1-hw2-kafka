863f3dfe6b94ad95905c233ab044651cb61f45f2
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2024-05-19 22:18:32 +0300

    feat: module metrics consumer added

---
+++ metrics-consumer/src/main/java/isthatkirill/hwtwokafka/consumer/ConsumerServiceApp.java
@@ -0,0 +1,13 @@
+package isthatkirill.hwtwokafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerServiceApp.class, args);
+    }
+
+}
